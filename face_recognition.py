896112bf8258300271d4a3f65b7f86c9410f1ce5
Captain-Gautam <gautamprajapati2252@gmail.com>
2023-01-23 17:53:05 +0530

    To detect the face and save it as attendace

--- face_recognition.py
+++ face_recognition.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 from tkinter import messagebox
 import mysql.connector
+from time import strftime
+from datetime import datetime
 from setuptools import Command
 import cv2
 #import tensorflow as tf
@@ -52,11 +54,26 @@ class Face_Recognition:
         b1_1 = Button(self.root, text="Face Recogniton", command=self.face_recog, cursor="hand2", font = ("cursive", 18, "italic"),bg ='#011f4b', fg = '#f8ae97')
         b1_1.place(x=10, y= 160, width = 200, height=50)
 
+    #===========Attendance CSV File================
+    def mark_attendance(self, i, e, n, d):
+        with open("Attendance.csv", "r+", newline="\n") as f:
+            myDatalist = f.readlines()
+            name_list = []
+            for line in myDatalist:
+                entry = line.split((","))
+                name_list.append(entry[0])
+            if((i not in name_list) and (n not in name_list) and (e not in name_list) and (d not in name_list)):
+                now=datetime.now()
+                d1 = now.strftime("%d/%m/%Y")
+                dtString = now.strftime("%H:%M:%S")
+                f.writelines(f"\n{i}, {e}, {n}, {d}, {dtString}, {d1}, Present")
+                
+            
 
     #==========Face Recognition Function=========
     def face_recog(self):
         
-        def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text, clf):   #scalefactor, miniNeighbours, color, text, clf
+        def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text, clf): 
             gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             features = classifier.detectMultiScale(gray_image, scaleFactor, minNeighbors)
 
@@ -75,29 +92,30 @@ class Face_Recognition:
                 my_cursor = conn.cursor()
 
                 my_cursor.execute("SELECT Student_Name FROM student where Student_Id="+str(id))
-                
                 n = my_cursor.fetchone()
-               
                 n="+".join(n)
 
-                my_cursor.execute("SELECT Enroll_No FROM student where Student_Id="+str(id)) 
+                my_cursor.execute("SELECT Enroll_No FROM student where Student_Id="+str(id)) #  my_cursor.execute("select Enroll_No from student where Enroll_No=%s"+str(id))
                 e = my_cursor.fetchone()
-               
                 e="+".join(e)
 
                 my_cursor.execute("SELECT Dep FROM student where Student_Id="+str(id))
-               
                 d = my_cursor.fetchone()
-                
                 d="+".join(d)
+                
+                
+                my_cursor.execute("SELECT Student_id FROM student where Student_Id="+str(id))
+                i = my_cursor.fetchone()
+                i="+".join(i)
 
    
 
                 if confidence > 77:
+                    cv2.putText(img, f"ID:{i}", (x, y-75), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
                     cv2.putText(img, f"Student_Name:{n}", (x, y-55), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
                     cv2.putText(img, f"Enroll_No:{e}", (x, y-30), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
                     cv2.putText(img, f"Dep:{d}", (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 0.8, (255, 255, 255), 3)
-
+                    self.mark_attendance(i, e, n, d)
                 else:
                     cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 3)
                
@@ -110,10 +128,11 @@ class Face_Recognition:
             return coord
 
         def recognize(img, clf, faceCascade):
-            coord = draw_boundary(img, faceCascade, 1.1, 10, (255, 25, 255), "Face", clf)  
+            coord = draw_boundary(img, faceCascade, 1.1, 10, (255, 25, 255), "Face", clf)  #draw_boundary(img, faceCascade, 1.1, 10, (255, 25, 255), "Face", clf)
             return img
 
 
+        #faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
         clf = cv2.face.LBPHFaceRecognizer_create()
