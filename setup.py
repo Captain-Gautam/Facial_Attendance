b86e560d0da8440e1dba9b8109e40d6a61fd7079
Captain-Gautam <103939546+Captain-Gautam@users.noreply.github.com>
2023-03-30 13:02:16 +0530

    Setup File for the Windows Desktop Application

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ if sys.platform == 'win32':
 os.environ['TCL_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python38\tcl\tcl8.6"
 os.environ['TK_LIBRARY'] = r"C:\Users\Admin\AppData\Local\Programs\Python\Python38\tcl\tk8.6"
 
-executables = [cx_Freeze.Executable("Face_Recognition_Software.py", base=base, icon="face.ico")]
+executables = [cx_Freeze.Executable("main.py", base=base, icon="face.ico")]
 
 
 cx_Freeze.setup(
     name = "Facial Recognition Software",
-    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["face.ico",'tcl86t.dll','tk86t.dll', 'college_images','data','database','attendance_report']}},
+    options = {"build_exe": {"packages":["tkinter","os"], "include_files":["face.ico",'tcl86t.dll','tk86t.dll', 'bg_images','data','database','Attendance']}},
     version = "1.0",
-    description = "Face Recognition Automatic Attendace System | Developed By CodeWithKiran",
+    description = "Face Recognition System | Developed By Gautam Prajapati",
     executables = executables
     )
